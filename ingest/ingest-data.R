2d639ffb81ae8c43d98bbd033f3745093787f50a
Louis-Philippe Brochu <lpbrochu@gmail.com>
2018-02-18 13:49:16 -0500

    changes to pipeline

---
+++ ingest/ingest-data.R
@@ -0,0 +1,11 @@
+# Read in `iris` data
+iris <- read.csv(url("http://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"), 
+                 header = FALSE) 
+
+# Print first lines
+head(iris)
+
+# Add column names
+names(iris) <- c("Sepal.Length", "Sepal.Width", "Petal.Length", "Petal.Width", "Species")
+
+write.csv(MyData, file = "~/data/iris.csv")
